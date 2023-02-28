846d27cecf8365b2fa12c9b5798269b145a2aff6
Leclerc Ludwig <ludwig.leclerc22@gmail.com>
2023-02-28 10:32:35 +0100

    lissage des lacs

--- minecraft2d.js
+++ minecraft2d.js
@@ -1,3 +1,15 @@
+function RGBToHex(color) {
+  let hex = color.toString(16);
+  return hex.length == 1 ? "0" + hex : hex;
+}
+
+function hexToRGB(hex) {
+  const r = parseInt(hex.slice(1, 3), 16);
+  const g = parseInt(hex.slice(3, 5), 16);
+  const b = parseInt(hex.slice(5, 7), 16);
+  return [r, g, b];
+}
+
 // Initialisation du canvas
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
@@ -6,6 +18,13 @@ const ctx = canvas.getContext("2d");
 const WIDTH = canvas.width;
 const HEIGHT = canvas.height;
 const BLOCK_SIZE = 16;
+const WATER_COLOR = "#C4E8E5";
+const GRASS_COLOR = "#28a745";
+const LAKE_CHANCE = 0.01;
+const LAKE_SIZE_MIN = 3;
+const LAKE_SIZE_MAX = 7;
+const LAKE_SMOOTHING = 0.6;
+const TREE_CHANCE = 0.05;
 
 // Génération du monde aléatoire
 const world = [];
@@ -13,26 +32,17 @@ for (let x = 0; x < WIDTH / BLOCK_SIZE; x++) {
   world[x] = [];
   for (let y = 0; y < HEIGHT / BLOCK_SIZE; y++) {
     // Couleur verte pour chaque bloc
-    world[x][y] = "rgb(17, 107, 24)";
-  }
-}
-
-// Génération des lacs
-const LAKE_CHANCE = 0.01; // Chance qu'un bloc soit un lac
-const LAKE_SIZE_MIN = 3; // Taille minimale du lac (en blocs)
-const LAKE_SIZE_MAX = 7; // Taille maximale du lac (en blocs)
-
-for (let x = 0; x < WIDTH / BLOCK_SIZE; x++) {
-  for (let y = 0; y < HEIGHT / BLOCK_SIZE; y++) {
-    if (Math.random() < LAKE_CHANCE) {
-      // Génération d'un lac de taille aléatoire
-      const lakeSize = Math.floor(Math.random() * (LAKE_SIZE_MAX - LAKE_SIZE_MIN + 1)) + LAKE_SIZE_MIN;
-      for (let i = 0; i < lakeSize; i++) {
-        for (let j = 0; j < lakeSize; j++) {
-          // Vérification que le bloc est dans le canvas
-          if (x + i < WIDTH / BLOCK_SIZE && y + j < HEIGHT / BLOCK_SIZE) {
-            // Couleur bleue pour chaque bloc du lac
-            world[x + i][y + j] = "blue";
+    world[x][y] = GRASS_COLOR;
+    
+    // Génération des arbres
+    if (Math.random() < TREE_CHANCE && world[x][y] === GRASS_COLOR) {
+      const treeSize = Math.floor(Math.random() * 3) + 3;
+      const treeX = x - Math.floor(treeSize / 2);
+      const treeY = y - treeSize + 1;
+      for (let i = treeX; i < treeX + treeSize; i++) {
+        for (let j = treeY; j <= y; j++) {
+          if (i >= 0 && i < WIDTH / BLOCK_SIZE && j >= 0 && j < HEIGHT / BLOCK_SIZE) {
+            world[i][j] = "#8B4513";
           }
         }
       }
@@ -40,28 +50,82 @@ for (let x = 0; x < WIDTH / BLOCK_SIZE; x++) {
   }
 }
 
+// Génération de l'eau
+for (let x = 0; x < WIDTH / BLOCK_SIZE; x++) {
+  for (let y = 0; y < HEIGHT / BLOCK_SIZE; y++) {
+    if (Math.random() < LAKE_CHANCE) {
+      const lakeSize = Math.floor(Math.random() * (LAKE_SIZE_MAX - LAKE_SIZE_MIN + 1)) + LAKE_SIZE_MIN;
+      for (let i = x; i < x + lakeSize; i++) {
+        for (let j = y; j < y + lakeSize; j++) {
+          if (i >= 0 && i < WIDTH / BLOCK_SIZE && j >= 0 && j < HEIGHT / BLOCK_SIZE) {
+            world[i][j] = "#C4E8E5";
+          }
+        }
+      }
+    }
+  }
+}
+
+// Lissage des lacs
+for (let x = 0; x < WIDTH / BLOCK_SIZE; x++) {
+  for (let y = 0; y < HEIGHT / BLOCK_SIZE; y++) {
+    if (world[x][y] === "#C4E8E5") {
+      let count = 0;
+      let sumR = 0;
+      let sumG = 0;
+      let sumB = 0;
+      for (let i = x - 1; i <= x + 1; i++) {
+        for (let j = y - 1; j <= y + 1; j++) {
+          if (i >= 0 && i < WIDTH / BLOCK_SIZE && j >= 0 && j < HEIGHT / BLOCK_SIZE) {
+            count++;
+            const [r, g, b] = hexToRGB(world[i][j]);
+            sumR += r;
+            sumG += g;
+            sumB += b;
+          }
+        }
+      }
+      const r = Math.round(sumR / count);
+      const g = Math.round(sumG / count);
+      const b = Math.round(sumB / count);
+      world[x][y] = "#ADD8E6";
+    }
+  }
+}
+
+
 // Position du joueur
 let playerX = 0;
 let playerY = 0;
 
+// Inventaire
+let inventoryOpen = false;
+
 // Contrôle du joueur
 window.addEventListener("keydown", (event) => {
   switch (event.code) {
-    case "ArrowUp":
-      playerY -= 1;
-      break;
-    case "ArrowDown":
-      playerY += 1;
-      break;
-    case "ArrowLeft":
-      playerX -= 1;
-      break;
-    case "ArrowRight":
-      playerX += 1;
+  case "ArrowUp":
+  if (playerY > 0 && world[playerX][playerY - 1] !== "#ADD8E6") {
+  playerY -= 1;
+  }
+  break;
+  case "ArrowDown":
+  if (playerY < HEIGHT / BLOCK_SIZE - 1 && world[playerX][playerY + 1] !== "#ADD8E6") {
+  playerY += 1;
+  }
+  break;
+  case "ArrowLeft":
+  if (playerX > 0 && world[playerX - 1][playerY] !== "#ADD8E6") {
+  playerX -= 1;
+  }
+  break;
+  case "ArrowRight":
+  if (playerX < WIDTH / BLOCK_SIZE - 1 && world[playerX + 1][playerY] !== "#C4E8E5") {
+  playerX += 1;
+  }
       break;
     case "KeyI":
-      // Ouvrir l'inventaire
-      console.log("Ouverture de l'inventaire");
+      inventoryOpen = !inventoryOpen;
       break;
   }
 });
@@ -81,4 +145,13 @@ setInterval(() => {
   // Dessiner le joueur
   ctx.fillStyle = "red";
   ctx.fillRect(playerX * BLOCK_SIZE, playerY * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
+
+  // Dessiner l'inventaire si ouvert
+  if (inventoryOpen) {
+    ctx.fillStyle = "rgba(0, 0, 0, 0.5)";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.font = "30px Arial";
+    ctx.fillStyle = "white";
+    ctx.fillText("Inventory", 20, 50);
+  }
 }, 1000 / 60);
